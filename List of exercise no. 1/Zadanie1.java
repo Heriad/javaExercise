4b69e283409b96ef001ca5de8bafe6396270f45d
Kamil Gańko <35296354+Heriad@users.noreply.github.com>
2018-05-18 22:33:58 +0200

    Add files via upload

---
+++ List of exercise no. 1/Zadanie1.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
+
+public class Zadanie1{
+	public static void main(String args[]){
+		
+		
+		
+}
+}
