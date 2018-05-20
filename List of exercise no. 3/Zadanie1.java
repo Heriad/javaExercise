8d29a36c4f95fc23d784ae0be66db7fca44fb6cd
Kamil Gańko <35296354+Heriad@users.noreply.github.com>
2018-05-20 19:44:32 +0200

    Add files via upload

---
+++ List of exercise no. 3/Zadanie1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Zadanie1{
+	public static void main(String args[]){
+		
+		int suma = 0;
+		int a = 6;
+		
+		for(int i = 1; a < 2000; i++)
+		{
+		    suma += a;
+		    System.out.println("a"+i+": "+a);
+			a += 9;
+		}
+		System.out.print("Suma wynosi: "+suma);
+	}
+}
